bb4cb6e0ff414e78b2a8d9b18b8904755c8b9004
mitchtabian <mitch@tabian.ca>
2016-04-24 21:53:51 -0700

    Source Code

---
+++ app/src/main/java/practice/application/graphviewtutorial1/MainActivity.java
@@ -0,0 +1,33 @@
+package practice.application.graphviewtutorial1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class MainActivity extends AppCompatActivity {
+
+    LineGraphSeries<DataPoint> series;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        double y,x;
+        x = -5.0;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph1);
+        series = new LineGraphSeries<DataPoint>();
+        for(int i =0; i<100; i++) {
+            x = x + 0.1;
+            y = Math.sin(x);
+            series.appendData(new DataPoint(x, y), true, 100);
+        }
+        graph.addSeries(series);
+    }
+
+
+}
